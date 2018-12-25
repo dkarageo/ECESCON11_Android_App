3fec6bd67866a01524a492f4f2181b7c484662b5
Dimitris Karageorgiou <soulrain@outlook.com>
2018-12-26 01:09:49 +0200

    Added support for accessing Context from App.

--- app/src/main/java/com/sfhmmy/mobile/App.java
+++ app/src/main/java/com/sfhmmy/mobile/App.java
@@ -1,5 +1,6 @@
 package com.sfhmmy.mobile;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.app.Application;
 
@@ -8,12 +9,15 @@ import android.app.Application;
  */
 public class App extends Application {
     private static Resources resources;
+    private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
         resources = getResources();
+        context = getApplicationContext();
     }
 
     public static Resources getAppResources() { return resources; }
+    public static Context getAppContext() { return context; }
 }
