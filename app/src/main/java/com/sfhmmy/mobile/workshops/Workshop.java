8271ecf0e24c69cf007220143b36b2390dc46a6b
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2019-03-18 14:00:24 +0200

    Added ID and EnrollStatus fields to Workshop.

--- app/src/main/java/com/sfhmmy/mobile/workshops/Workshop.java
+++ app/src/main/java/com/sfhmmy/mobile/workshops/Workshop.java
@@ -15,25 +15,38 @@ import org.threeten.bp.ZonedDateTime;
 
 
 public class Workshop {
+    public enum EnrollStatus {
+        AVAILABLE,
+        UNAVAILABLE,
+        ACCEPTED,
+        REJECTED,
+        PENDING
+    }
 
+    private int           mId;
     private String        mName;
     private String        mDescription;
     private String        mImageUrl;
     private String        mJoinQuestion;
     private String        mPlace;
     private ZonedDateTime mDateTime;
+    private EnrollStatus  mEnrollStatus;
 
+    public int getId() { return mId; }
     public String getName() { return mName; }
     public String getDescription() { return mDescription; }
     public String getImageUrl() { return mImageUrl; }
     public String getJoinQuestion() { return mJoinQuestion; }
     public String getPlace() { return mPlace; }
     public ZonedDateTime getDateTime() { return mDateTime; }
+    public EnrollStatus getEnrollStatus() { return mEnrollStatus; }
 
+    public void setId(int id) { mId = id; }
     public void setName(String name) { mName = name; }
     public void setDescription(String description) { mDescription = description; }
     public void setImageUrl(String imageUrl) { mImageUrl = imageUrl; }
     public void setJoinQuestion(String joinQuestion) { mJoinQuestion = joinQuestion; }
     public void setPlace(String place) { mPlace = place; }
     public void setDateTime(ZonedDateTime dateTime) { mDateTime = dateTime; }
+    public void setEnrollStatus(EnrollStatus enrollStatus) { mEnrollStatus = enrollStatus; }
 }
