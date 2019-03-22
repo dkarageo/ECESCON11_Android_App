4c7a4be901f66ffa579f674b9c318e52cea7c1ea
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2019-03-22 22:15:19 +0200

    Implemented Passport Fragment as user aware fragment.

--- app/src/main/java/com/sfhmmy/mobile/users/PassportFragment.java
+++ app/src/main/java/com/sfhmmy/mobile/users/PassportFragment.java
@@ -14,19 +14,24 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.sfhmmy.mobile.R;
+import com.sfhmmy.mobile.UserAwareFragment;
 import com.sfhmmy.mobile.TopLevelFragmentEventsListener;
+
+import androidx.annotation.NonNull;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -37,15 +42,19 @@ import com.sfhmmy.mobile.TopLevelFragmentEventsListener;
  * It can be used as the main content of an activity, by setting that activity as a
  * TopLevelFragmentEventsListener.
  */
-public class PassportFragment extends Fragment {
+public class PassportFragment extends UserAwareFragment {
 
     private TopLevelFragmentEventsListener mTopListener;
 
-    private ImageView mQRCode;
-    private ImageView mProfilePicture;
-    private TextView  mUserName;
-    private TextView  mUserEmail;
-    private TextView  mUserOrg;
+    private ImageView       mQRCode;
+    private CircleImageView mProfilePicture;
+    private TextView        mNameSurname;
+    private TextView        mUserEmail;
+    private TextView        mUserOrg;
+    private TextView        mUserRole;
+    private View            mUserCardContainer;
+    private View            mUserMissingCardContainer;
+    private Button          mLoginButton;
 
     private Bitmap qrImg = null;  // Bitmap of QR Code, generated once upon fragment's creation.
 
@@ -72,44 +81,33 @@ public class PassportFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_passport, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        View root = inflater.inflate(R.layout.fragment_passport, container, false);
 
         // Acquire handlers to UI elements.
-        mQRCode         = (ImageView) getActivity().findViewById(R.id.passport_qrcode);
-        mProfilePicture = (ImageView) getActivity().findViewById(R.id.passport_profile_img);
-        mUserName       = (TextView)  getActivity().findViewById(R.id.passport_name);
-        mUserEmail      = (TextView)  getActivity().findViewById(R.id.passport_email);
-        mUserOrg        = (TextView)  getActivity().findViewById(R.id.passport_organization);
+        mQRCode                   = root.findViewById(R.id.passport_qrcode);
+        mProfilePicture           = root.findViewById(R.id.passport_profile_img);
+        mNameSurname              = root.findViewById(R.id.passport_name_surname);
+        mUserEmail                = root.findViewById(R.id.passport_email);
+        mUserOrg                  = root.findViewById(R.id.passport_organization);
+        mUserRole                 = root.findViewById(R.id.passport_role);
+        mUserCardContainer        = root.findViewById(R.id.passport_user_card_container);
+        mUserMissingCardContainer = root.findViewById(R.id.passport_user_missing_card_container);
+        mLoginButton              = root.findViewById(R.id.passport_unlogged_user_notification_login_button);
 
-        // Fill UI elements with content.
-        User curUser = UserManager.getUserManager().getCurrentUser();
-        if (curUser != null) {
-            String email = curUser.getEmail();
-            String name  = curUser.getName();
-            String org   = curUser.getOrganization();
-            Bitmap prImg = curUser.getProfilePicture();
-
-            // The first time fragment is created, generate a QRCode Bitmap.
-            // TODO: Decouple QR Bitmap generation from fragment's lifecycle.
-            if (qrImg == null) qrImg = email != null ? generateQRCodeBitmap(email) : null;
-
-            if (prImg != null) mProfilePicture.setImageBitmap(prImg);
-            if (qrImg != null) mQRCode.setImageBitmap(qrImg);
-            if (name  != null) mUserName.setText(name);
-            if (email != null) mUserEmail.setText(curUser.getEmail());
-            if (org   != null) mUserOrg.setText(org);
-        }
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayLoginDialog();
+            }
+        });
 
         // Make sure that profile picture is not hidden by qr code wrapper.
         mProfilePicture.bringToFront();
+
+        return root;
     }
 
     @Override
@@ -122,6 +120,66 @@ public class PassportFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mTopListener = null;
+    }
+
+    @Override
+    protected void onCreateUserSpecificContent(User user) {
+        super.onCreateUserSpecificContent(user);
+
+        if (user != null) displayUserCard(user);
+        else displayUserMissingCard();
+    }
+
+    private void displayUserCard(User user) {
+        // Display the user's card.
+        mUserCardContainer.setVisibility(View.VISIBLE);
+        mUserMissingCardContainer.setVisibility(View.GONE);
+
+        // Fill in user's detail.
+        String email   = user.getEmail();
+        String name    = user.getName();
+        String surname = user.getSurname();
+        String org     = user.getOrganization();
+
+        // The first time fragment is created, generate a QRCode Bitmap.
+        // TODO: Decouple QR Bitmap generation from fragment's lifecycle.
+        if (qrImg == null) qrImg = email != null ? generateQRCodeBitmap(email) : null;
+
+        if (qrImg != null) mQRCode.setImageBitmap(qrImg);
+        if (name  != null) mNameSurname.setText(String.format("%s %s", name, surname));
+        if (email != null) mUserEmail.setText(email);
+        if (org   != null) mUserOrg.setText(org);
+
+        updateUserRole(user);
+
+        Glide.with(mProfilePicture)
+                .load(user.getProfilePictureURL())
+                .dontAnimate()
+                .into(mProfilePicture);
+    }
+
+    private void displayUserMissingCard() {
+        mUserCardContainer.setVisibility(View.GONE);
+        mUserMissingCardContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void updateUserRole(User user) {
+
+        switch (user.getRole()) {
+            case ADMINISTRATOR:
+                mUserRole.setText(getString(R.string.passport_user_role_administrator_text));
+                mUserRole.setVisibility(View.VISIBLE);
+                break;
+
+            case SECRETARY:
+                mUserRole.setText(getString(R.string.passport_user_role_secretary_text));
+                mUserRole.setVisibility(View.VISIBLE);
+                break;
+
+            case VISITOR:
+                mUserRole.setVisibility(View.GONE);
+                break;
+        }
     }
 
     /**
