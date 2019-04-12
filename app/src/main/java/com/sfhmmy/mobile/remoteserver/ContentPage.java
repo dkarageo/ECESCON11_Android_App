19cd7e939ea05d669984949e543c0a24fe59b9d8
Dimitris Karageorgiou (University Repo) <soulrain@outlook.com>
2019-04-12 18:42:02 +0300

    Implemented Serializable to ContentPage.

--- app/src/main/java/com/sfhmmy/mobile/remoteserver/ContentPage.java
+++ app/src/main/java/com/sfhmmy/mobile/remoteserver/ContentPage.java
@@ -11,10 +11,15 @@
 
 package com.sfhmmy.mobile.remoteserver;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class ContentPage<T> {
+public class ContentPage<T> implements Serializable {
 
     private List<T> mContentList;
     private int mCurrentPage;
@@ -31,4 +36,19 @@ public class ContentPage<T> {
     public void setCurrentPage(int currentPage) { mCurrentPage = currentPage; }
     public void setTotalPages(int totalPages) { mTotalPages = totalPages; }
     public void setItemsPerPage(int itemsPerPage) { mitemsPerPage = itemsPerPage; }
+
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        oos.defaultWriteObject();
+        if (!(mContentList instanceof Serializable)) {
+            ArrayList<T> serializableList = new ArrayList<>(mContentList);
+            oos.writeObject(serializableList);
+        } else {
+            oos.writeObject((Serializable) mContentList);
+        }
+    }
+
+    private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+        ois.defaultReadObject();
+        mContentList = (List<T>) ois.readObject();
+    }
 }
